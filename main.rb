84829ea59d4e1a3a64ba22166c5380768bfd740f
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-10-18 14:20:14 -0500

    Update main.rb
    creating the person class and the of_age methods? and can_use_services?

--- main.rb
+++ main.rb
@@ -2,21 +2,23 @@
 
 # Create class Person
 class Person
-  attr_accessor :name
-  attr_reader :age, :id
+  attr_accessor :name, :age
+  attr_reader :id
 
-  attr_writer age
-
-  def initialize(age, id, name = 'Unknown', parent_permission = 'true')
+  def initialize(age, id, name: 'Unknown', parent_permission: true)
     @id = id
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
+  def can_use_services?
+    @age >= 18 || @parent_permission
+  end
+
   private
 
   def of_age?
-    @age > 18
+    @age < 18
   end
 end
