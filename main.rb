5b3b2e6af6fec4a59b1e3711c7a2c834223b6078
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-10-17 21:10:35 -0500

    Update main.rb
    Creating class person

--- main.rb
+++ main.rb
@@ -1,3 +1,22 @@
 # frozen_string_literal: true
 
-puts 'hola'
+# Create class Person
+class Person
+  attr_accessor :name
+  attr_reader :age, :id
+
+  attr_writer age
+
+  def initialize(age, id, name = 'Unknown', parent_permission = 'true')
+    @id = id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age > 18
+  end
+end
