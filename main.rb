180023ec690368e2ee5166e336063fb2ac3cf334
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-10-19 17:00:43 -0500

    Update main.rb
    creating the Nameable, Base Decorator, Uppercase Decorator, and Clipper Decorator classes.
    Creating the correct_nam methods for each class

--- main.rb
+++ main.rb
@@ -1,17 +1,27 @@
-# Create class Person
-class Person
+class Nameable
+  def correct_name
+    raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+  end
+end
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, id, name: 'Unknown', parent_permission: true)
-    @id = id
+    super()
     @name = name
     @age = age
+    @id = id
     @parent_permission = parent_permission
   end
 
   def can_use_services?
     @age >= 18 || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   private
@@ -20,3 +30,39 @@ class Person
     @age < 18
   end
 end
+
+class BaseDecorator < Nameable
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < BaseDecorator
+  def correct_name
+    original_name = @nameable.correct_name
+    original_name.capitalize
+  end
+end
+
+class TrimmerDecorator < BaseDecorator
+  def correct_name
+    original_name = @nameable.correct_name
+    if original_name.length > 10
+      original_name[0, 10]
+    else
+      original_name
+    end
+  end
+end
+
+person = Person.new(22, 1, name: 'maximilianus')
+person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
