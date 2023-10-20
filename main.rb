577980ec2b6b35816c994be207fc9932e6bff4cf
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-10-20 16:32:37 -0500

    Update main.rb
    Fixing classes and associations according to what was requested in the PR

--- main.rb
+++ main.rb
@@ -5,29 +5,28 @@ class Nameable
 end
 
 class Person < Nameable
-  attr_accessor :name, :age
   attr_reader :id
+  attr_accessor :name, :age, :rentals
 
-  def initialize(age, id, name: 'Unknown', parent_permission: true)
+  def initialize(age:, name: 'Unknown', parent_permission: true)
     super()
+    @id = nil
     @name = name
     @age = age
-    @id = id
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services?
-    @age >= 18 || @parent_permission
+    of_age? || @parent_permission
   end
 
   def correct_name
     @name
   end
 
-  private
-
-  def of_age?
-    @age < 18
+  def add_rental(book, date)
+    Rental.new(date, book, self)
   end
 end
 
@@ -60,7 +59,7 @@ class TrimmerDecorator < BaseDecorator
   end
 end
 
-person = Person.new(22, 1, name: 'maximilianus')
+person = Person.new(age: 22, name: 'maximilianus', parent_permission: true)
 person.correct_name
 capitalized_person = CapitalizeDecorator.new(person)
 puts capitalized_person.correct_name
@@ -68,23 +67,16 @@ capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
 puts capitalized_trimmed_person.correct_name
 
 class Student
-  attr_accessor :name
-  attr_reader :classroom
+  attr_accessor :name, :classroom
 
   def initialize(name)
     @name = name
     @classroom = nil
   end
-
-  def classroom(classroom)
-    @classroom = classroom
-    classroom.students.push(self) unless classroom.students.include?(self)
-  end
 end
 
 class Classroom
-  attr_accessor :label
-  attr_reader :students
+  attr_accessor :label, :students
 
   def initialize(label)
     @label = label
@@ -98,26 +90,26 @@ class Classroom
 end
 
 class Book
-  attr_accessor :title, :author
-  attr_reader :rentals
+  attr_accessor :title, :author, :rentals
 
   def initialize(title, author)
     @title = title
     @author = author
-    @rentals = [] # Initialize the rentals array
+    @rentals = []
+  end
+
+  def add_rental(person, date)
+    @rentals << Rental.new(date, self, person)
   end
 end
 
 class Rental
-  attr_accessor :date
-  attr_reader :book, :person
+  attr_accessor :date, :book, :person
 
   def initialize(date, book, person)
     @date = date
     @book = book
     @person = person
-    book.rentals << self
-    person.rentals << self
   end
 end
 
@@ -131,12 +123,9 @@ classroom.add_student(student2)
 
 puts "Students in #{classroom.label}: #{classroom.students.map(&:name).join(', ')}"
 
-book1 = Book.new('Book 1', 'Author 1')
-book2 = Book.new('Book 2', 'Author 2')
-person1 = Student.new('Person 1')
-person2 = Student.new('Person 2')
+book1 = Book.new('100 años de soledad', 'Gabriel Garcia Marquez')
+person1 = Person.new(age: 30, name: 'Carlos')
+
 rental1 = Rental.new('2023-10-19', book1, person1)
-rental2 = Rental.new('2023-10-20', book2, person2)
 
 puts "#{person1.name} rented '#{book1.title}' on #{rental1.date}."
-puts "#{person2.name} rented '#{book2.title}' on #{rental2.date}."
