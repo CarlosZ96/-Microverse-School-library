fa7593d7fc055ba729b18cbf923d3fbc4fba69f2
Jlberbesi <jlberbesi.walloom@gmail.com>
2023-11-03 23:19:45 -0500

    Fix classroom.rb

--- classroom.rb
+++ classroom.rb
@@ -1,17 +1,19 @@
-require_relative 'person'
 require_relative 'student'
-
 class Classroom
   attr_accessor :label
   attr_reader :students
-
   def initialize(label)
     @label = label
     @students = []
   end
-
   def add_student(student)
-    @students << student
-    student.classroom = self
+    @students << student unless @students.include?(student)
+    student.classroom = self unless student.classroom == self
+  end
+  def to_hash
+    {
+      label: @label,
+      students: @students.map(&:id)
+    }
   end
 end
