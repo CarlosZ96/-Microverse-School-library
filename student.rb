db3699767003c6dbe5b8bef73b1083f42bf1b84a
Jlberbesi <jlberbesi.walloom@gmail.com>
2023-11-03 23:27:02 -0500

    Fix student.rb

--- student.rb
+++ student.rb
@@ -1,20 +1,23 @@
 require_relative 'person'
 require_relative 'classroom'
-
 class Student < Person
   attr_reader :classroom
-
-  def initialize(age, name, classroom, parent_permission: true)
-    super(age, name, parent_permission: parent_permission)
-    @classroom = classroom
+  def initialize(age, name, classroom_label, parent_permission: true, id: nil)
+    super(age, name, parent_permission: parent_permission, id: id)
+    @classroom = Classroom.new(classroom_label)
+    classroom.add_student(self)
   end
-
   def play_hooky
-    '¯(ツ)/¯'
+    '¯\(ツ)/¯'
   end
-
   def classroom=(classroom)
     @classroom = classroom
     classroom.add_student(self) unless classroom.students.include?(self)
+  end
+  def to_hash
+    super.merge({
+      classroom: @classroom.label,
+      type: 'Student'
+    })
   end
 end
