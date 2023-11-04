ac5738604759b7aba79c8cb3c8758e6cc4f126d0
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-11-04 18:08:00 -0500

    Feature:
    Addinf Rental, Student and Teacher file whit test

---
+++ rental_spec.rb
@@ -0,0 +1,21 @@
+require_relative ‘rental’
+require_relative ‘person’
+require_relative ‘book’
+describe Rental do
+  let(:person) { Person.new(22, 'Max Mustermann') }
+  let(:book) { Book.new('The Pragmatic Programmer', 'Andy Hunt & Dave Thomas') }
+  let(:rental) { Rental.new('2023-04-01', book, person) }
+  describe '#new' do
+    it 'creates a new rental with a date, book, and person' do
+      expect(rental).to have_attributes(date: '2023-04-01')
+      expect(rental.book).to eq book
+      expect(rental.person).to eq person
+    end
+  end
+  describe 'associations' do
+    it 'associates a book with a person' do
+      expect(book.rentals).to include(rental)
+      expect(person.rentals).to include(rental)
+    end
+  end
+end
