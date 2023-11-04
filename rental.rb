be25b2e22c8e4c556cceb3f4ecfbc6c440e4e36e
Jlberbesi <jlberbesi.walloom@gmail.com>
2023-11-03 23:25:51 -0500

    Fix rental.rb

--- rental.rb
+++ rental.rb
@@ -1,15 +1,19 @@
 require_relative 'book'
 require_relative 'person'
-
 class Rental
   attr_accessor :date, :book, :person
-
   def initialize(date, book, person)
     @date = date
     @book = book
     @person = person
-
-    book.rentals << self
-    person.rentals << self
+    book.rentals << self unless book.rentals.include?(self)
+    person.rentals << self unless person.rentals.include?(self)
+  end
+  def to_hash
+    {
+      date: @date,
+      book: @book.title,
+      person_id: @person.id
+    }
   end
 end
