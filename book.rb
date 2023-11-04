50e612bf1e40905791e93feb7e16e0f7f72c2bb4
Jlberbesi <jlberbesi.walloom@gmail.com>
2023-11-03 23:18:41 -0500

    Fix book.rb code

--- book.rb
+++ book.rb
@@ -1,15 +1,18 @@
 require_relative 'rental'
-
 class Book
   attr_accessor :title, :author, :rentals
-
   def initialize(title, author)
     @title = title
     @author = author
     @rentals = []
   end
-
   def add_rental(person, date)
     @rentals << Rental.new(date, self, person)
+  end
+  def to_hash
+    {
+      title: @title,
+      author: @author
+    }
   end
 end
