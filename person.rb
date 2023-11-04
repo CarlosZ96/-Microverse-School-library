f237206076018d9e0f66803d9659fc03a8c24cb1
Jlberbesi <jlberbesi.walloom@gmail.com>
2023-11-03 23:24:29 -0500

    Fix person.rb

--- person.rb
+++ person.rb
@@ -1,26 +1,28 @@
 require_relative 'rental'
-
 class Person
   attr_reader :id, :name, :age, :rentals
-
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = rand(1..1000)
+  def initialize(age, name = 'Unknown', parent_permission: true, id: nil)
+    @id = id || rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
     @rentals = []
   end
-
   def can_use_services?
     of_age? || @parent_permission
   end
-
   def add_rentals(book, date)
     @rentals << Rental.new(date, book, self)
   end
-
+  def to_hash
+    {
+      id: @id,
+      name: @name,
+      age: @age,
+      parent_permission: @parent_permission
+    }
+  end
   private
-
   def of_age?
     @age >= 18
   end
